138e0074866c03168de4b00c5a20e5405b593817
Adriano Schmidt <adriano@localhost8080.com.br>
2014-12-27 20:05:37 -0200

    integraçoes com bd

--- src/main/java/br/com/pdionline/ejb/UserService.java
+++ src/main/java/br/com/pdionline/ejb/UserService.java
@@ -1,40 +1,27 @@
 package br.com.pdionline.ejb;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.pdionline.entity.User;
 
 @Stateless
 public class UserService {
 
-	public List<User> findUsers() {
+	@PersistenceContext
+	private EntityManager em;
 
-		List<User> users = new ArrayList<User>();
-
-		User user1 = new User();
-		user1.setId(1L);
-		user1.setName("Carlos Silva Fernandes");
-		users.add(user1);
-
-		User user2 = new User();
-		user2.setId(2L);
-		user2.setName("José Silveira Duffeck");
-		users.add(user2);
-
-		User user3 = new User();
-		user3.setId(3L);
-		user3.setName("Marcio Pereira Rosa");
-		users.add(user3);
-
-		User user4 = new User();
-		user4.setId(4L);
-		user4.setName("Manoella Assis Garcez");
-		users.add(user4);
+	public List<User> findAll() {
+		List<User> users = this.em.createQuery("SELECT o FROM User o").getResultList();
 
 		return users;
+	}
 
+	public User getById(Long userEvaluatedId) {
+		User user = this.em.find(User.class, userEvaluatedId);
+		return user;
 	}
 }
