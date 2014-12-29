2883c90504548dc5d46f470dbb5fbc41cfc6c639
guilherme-ribeiro <guilhermedoceu@gmail.com>
2014-12-29 19:26:27 -0200

    busca de usuarios

--- src/main/java/br/com/pdionline/ejb/UserService.java
+++ src/main/java/br/com/pdionline/ejb/UserService.java
@@ -1,10 +1,12 @@
 package br.com.pdionline.ejb;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import br.com.pdionline.entity.User;
 
@@ -26,9 +28,42 @@ public class UserService {
 
 	}
 	
-	public void create(User user){
+	public String create(User user){
+
+		boolean jaExiste = false;
+
+		Object object = em.createQuery("" +
+				"select count(o.id) " +
+				"from User o " +
+				"where o.email = :email " +
+				"or o.name = :nome ")
+				.setParameter("email",user.getEmail())
+				.setParameter("nome",user.getName())
+				.getSingleResult();
 		
-		em.persist(user);
+		
+
+		if(jaExiste){
+			return "Usuário já existe";
+		}else{
+			em.persist(user);
+		}
+
+		
+		return null;
 		
 	}
+	
+	public List<User> buscaPorNome(String query){
+		
+		List<User> users = new ArrayList<>();
+		Query usersByName = em.createQuery("select u from User u where u.name like :query");
+		usersByName.setParameter("query","%"+query+"%");
+		
+		users = usersByName.getResultList();
+		
+		return users;
+		
+	}
+	
 }
--- src/main/java/br/com/pdionline/jsf/converter/TeamMemberMB.java
+++ src/main/java/br/com/pdionline/jsf/converter/TeamMemberMB.java
@@ -2,9 +2,18 @@ package br.com.pdionline.jsf.converter;
 
 import br.com.pdionline.ejb.UserService;
 import br.com.pdionline.entity.User;
+import org.primefaces.model.DashboardColumn;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.DefaultDashboardColumn;
+import org.primefaces.model.DefaultDashboardModel;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gui on 28/12/14.
@@ -16,18 +25,53 @@ public class TeamMemberMB {
     private UserService userService;
     
     private User user = new User();
+    
+    private DashboardModel model;
+    
+    private List<User> autoCompleteUsers;
 
-    public  void  save(){
+    public  String  save(){
 
-        userService.create(user);
+        String errors = userService.create(user);
+        if (errors != null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(errors));
+            return null;
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuário adicionado com sucesso."));
+        return null;
 
     }
+    
 
+
+    public List<User> completMethodUser(String query){
+
+        return userService.buscaPorNome(query);
+        
+    }
+    /* GETTERS N SETTERS*/
+    
     public User getUser() {
         return user;
     }
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public DashboardModel getModel() {
+        return model;
+    }
+
+    public void setModel(DashboardModel model) {
+        this.model = model;
+    }
+
+    public List<User> getAutoCompleteUsers() {
+        return autoCompleteUsers;
+    }
+
+    public void setAutoCompleteUsers(List<User> autoCompleteUsers) {
+        this.autoCompleteUsers = autoCompleteUsers;
     }
 }
